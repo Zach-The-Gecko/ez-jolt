152ea001b08385d396520fc0349d8d3f368362f8
Zach Geckler <Zach.The.Gecko@gmail.com>
2026-05-20 15:26:07 -0600

    Use website instead of localhost

--- public/script.js
+++ public/script.js
@@ -20,7 +20,7 @@ const submitButtonClickHandler = async () => {
     const encodedData = encodeURI(requestData);
     const response = await fetch(
       // `https://joltplus.onrender.com/get-shifts-for-date-range?data=${encodedData}`
-      `http://localhost:3000/get-shifts-for-date-range?data=${encodedData}`,
+      `https://joltplus.onrender.com/get-shifts-for-date-range?data=${encodedData}`,
     );
 
     if (!response.ok) {
